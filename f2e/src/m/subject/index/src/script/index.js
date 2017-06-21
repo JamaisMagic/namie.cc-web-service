2567d6e198d5abc5ddebf6ec0e70ef03f16c479a
JamaisMagic <1350756140@qq.com>
2017-06-21 19:32:29 +0800

    update style of main page.

--- f2e/src/m/subject/index/src/script/index.js
+++ f2e/src/m/subject/index/src/script/index.js
@@ -1,17 +1,24 @@
 import 'cc.namie.jamais.m/src/style/common/common.styl';
+import '../style/common.styl';
 import '../style/index.styl';
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import { default as mCommon} from 'cc.namie.jamais';
 
 import {getAxiosInstance} from './modules/request';
 import {URL_PATH} from './modules/request';
 
+import GhIcon from 'cc.namie.jamais/src/component/ghIcon/GhIcon';
+
 const mAxios = getAxiosInstance();
+
+Vue.use(Vuetify);
 
 new Vue({
     el: '#app',
     data() {
         return {
+            is_requesting: true,
             url: '',
             res_url: ''
         };
@@ -19,15 +26,23 @@ new Vue({
     created() {
 
     },
+    mounted() {
+        this.is_requesting = false;
+    },
+    components: {
+        GhIcon
+    },
     methods: {
         onSubmit() {
             this.shorten(this.url);
         },
         shorten(url) {
             const self = this;
+            this.is_requesting = true;
             mAxios.post(mCommon.util.getNamieApi(URL_PATH.SHORTEN), {url})
                 .then(res => res.data || {})
                 .then(data => {
+                    this.is_requesting = false;
                     if (data.code === 0) {
                         self.res_url = data.data.url;
                         return;
@@ -35,6 +50,7 @@ new Vue({
                     alert(`${data.code} : ${data.msg}`);
                 })
                 .catch(err => {
+                    this.is_requesting = false;
                     console.log(err);
                     alert(err);
                 })
